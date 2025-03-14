67c5dad05901a5abfaf8667135a63113b8c2b79e
Steven <st.latuszek@gmail.com>
2025-03-14 11:47:05 +0100

    handle absolute and relative paths for image and save

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,26 @@ mod quantize;
 mod yaml;
 use std::env;
 
+fn handle_path(path: String) -> String {
+    if path.starts_with("/") || path.starts_with("\\") {
+        path
+    } else {
+        let current_dir = std::env::current_dir().unwrap().display().to_string();
+        format!("{}/{}", current_dir, path)
+    }
+}
+
 fn main() {
     let mut args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         panic!("Insufficient arguments provided. An image and save file locations required");
     }
-    let save = args.pop().expect("No save location provided");
+    let mut save = args.pop().expect("No save location provided");
     let image = args.pop().expect("No image provided");
 
     let config = config::parse_config(args);
 
-    let current_dir = env::current_dir().unwrap().display().to_string();
-    let path = format!("{}/{}", current_dir, image);
+    let path = handle_path(image);
     println!("Reading image: {}", path);
     let img = image::open(path).expect("Could not open image");
     println!("Opened");
@@ -36,5 +44,7 @@ fn main() {
             println!("No color number {}", color.0);
         }
     }
+    save = handle_path(save);
+    println!("Saving scheme to {}", save);
     yaml::export_scheme(&save, &base16);
 }
